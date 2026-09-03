69d54d3a823e3674b77e4e39ca5b91c8a24119dd
apostoliskaramichalisak-gif <apostolis.karamichalis.ak@gmail.com>
2026-09-04 01:24:52 +0300

    Create TapCard Pro JavaScript

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+// TapCard Pro v2.0 - Frontend script
+(function() {
+    console.log('TapCard Pro loaded');
+    
+    if (typeof TapCardData !== 'undefined') {
+        console.log('Profile data:', TapCardData);
+    }
+})();
